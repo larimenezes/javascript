8faebf182545d4a7e0949907ad54da3ed23eddb2
Larissa <larissasezenem@gmail.com>
2023-04-12 18:00:49 -0300

    update

---
+++ ex016/script.js
@@ -0,0 +1,30 @@
+function contar() {
+    let ini = document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    let res = document.querySelector('div#res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)    
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1!')
+            p = 1
+        }
+        if (i < f) {
+            for(let c = i;c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i;c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } 
+
+}
