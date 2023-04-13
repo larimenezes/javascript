3da40b69eed9834c08ea4c3522693ba711597df7
Larissa <larissasezenem@gmail.com>
2023-04-13 17:18:57 -0300

    update

--- ex015/script.js
+++ ex015/script.js
@@ -2,9 +2,9 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
-    var res = window.document.querySelector('div#res')
+    var res = window.document.getElementById('res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+        window.alert('[erro] Verifique os dados e tente novamente!')
     } else {
         var fsex = window.document.getElementsByName('radiosex')
         var idade = ano - Number(fano.value)
@@ -14,33 +14,32 @@ function verificar() {
         if (fsex[0].checked) {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'ftbbmenino.png')
             } else if (idade < 21) {
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'ftjovemhomem.png')
             } else if (idade < 50) {
-                img.setAttribute('src', '')
-            } else { 
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'fthomemadulto.png')
+            } else {
+                img.setAttribute('src', 'ftidoso.png')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'ftbbmenina.png')
             } else if (idade < 21) {
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'ftjovemmulher.png')
             } else if (idade < 50) {
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'ftmulheradulta.png')
             } else {
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'ftidosa.png')
             }
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos!`
+        res.textAlign = 'center'   
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
         res.appendChild(img)
     }
-
+    
 }
-
 
 
 
