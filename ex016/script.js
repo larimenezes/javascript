880c467ad6287f433d55de25686f3471879cffad
Larissa <larissasezenem@gmail.com>
2023-04-14 16:13:16 -0300

    update

--- ex016/script.js
+++ ex016/script.js
@@ -1,28 +1,31 @@
-function  contar() {
-    let ini = document.querySelector('input#txti')
-    let fim = document.querySelector('input#txtf')
-    let passo = document.querySelector('input#txtp')
-    let res = document.querySelector('div#res')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = `[erro] Verifique e digite novamente!`
+        res.innerHTML = `[ERRO] Digite novamente!`
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br> '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
         if (p <= 0) {
-            window.alert('Passo inválido! Considerando PASSO 1')
-            p = 1
-        }
+        window.alert('Número inválido! Vamos considerar passo 1!')
+        p = 1
+        } 
         if (i < f) {
-            for(let c = i;c <= f; c += p) {
-                res.innerHTML += `${c}  \u{1F449}`
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c}`
             }
+            
         } else {
-            for(let c = i;c >= f;c -= p) {
-            res.innerHTML += `${c}  \u{1F449}`
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += `${c}`
             }
         }
         res.innerHTML += `\u{1F3C1}`
-    } 
+
+    }
+
 }
